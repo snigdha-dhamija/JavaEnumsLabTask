19ce3fc0ce77da2560fa54c58968e6b9ef154814
snigdha-dhamija <67826444+snigdha-dhamija@users.noreply.github.com>
2020-10-28 20:13:05 +0530

    Update Months.java
    Enum

--- src/main/Months.java
+++ src/main/Months.java
@@ -1,5 +1,54 @@
-package main;
+package labWork.labWork6;
 
 public enum Months {
-    
+    JANUARY(31),
+    FEBRUARY(29),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DECEMBER(31);
+
+    private final int numberOfDays;
+
+    Months(int numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
+
+    public int getNumberOfDays() {
+        return numberOfDays;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
